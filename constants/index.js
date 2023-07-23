8c92a78d5831fe0283a3d02b1a7bfb2fdded492f
ghavidelmhc <42114768+ghavidelmhc@users.noreply.github.com>
2023-07-23 21:04:58 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x22368e99c537efdbd9ca413cb19ec944ae143d6f";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
